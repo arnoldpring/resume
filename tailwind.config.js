0de35a82e43785c80da0e5be3946a4b83f16166d
arnoldpring <arnold.xpanse@gmail.com>
2020-03-25 15:25:41 +0800

    redo mobile first

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,24 @@ module.exports = {
       center: true,
       padding: '2rem'
     },
-    
+    extend: {
+    //   screens: {
+        
+    //     'xs':{'min':'500px'},
+    //     '2xs':{'min':'425px'},
+    //     '3xs':{'min':'375px'},
+    //     '4xs':{'min':'320px'}
+    // },
+    height: {
+       28: '7rem',
+    },
+    fontSize: {
+      'tiny': '0.95rem',
+    },
   },
+    
   variants: {},
   plugins: [],
+}
+
 }
